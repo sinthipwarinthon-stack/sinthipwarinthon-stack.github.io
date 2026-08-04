cc313175f571d4d333060f898c220b41d6c5da4c
sinthipwarinthon-stack <sinthip.warinthon@gmail.com>
2026-08-04 12:00:48 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
 // เพิ่ม JavaScript สำหรับการโต้ตอบกับผู้ใล้ (Optional)
 document.addEventListener('DOMContentLoaded',function () {
-   console.log('Portfolio ของ SOMCHAI พร้อมแล้ว!');
+   console.log('Portfolio ของ Warinthon พร้อมแล้ว!');
 }};
