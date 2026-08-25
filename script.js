c310917cdb48456750dcd1c08fe9cea7d09746d2
sinthipwarinthon-stack <sinthip.warinthon@gmail.com>
2026-08-25 12:42:21 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
 document.addEventListener('DOMContentLoaded',function () {
    document.getEIementById('year').innerHTML= new Date().getFullYear();
    console.log('Portfolio ของ Warinthon พร้อมแล้ว!');
-}};
+});
